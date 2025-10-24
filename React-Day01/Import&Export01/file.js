57f4eed6de5ec1cf1eb9aa348bd385328038d0bf
Angad Anil Bari <angad@Angads-MacBook-Air.local>
2025-10-25 00:26:07 +0530

    Day-01

---
+++ React-Day01/Import&Export01/file.js
@@ -0,0 +1,15 @@
+
+//default
+
+const Man1="Irom-Man";
+
+
+//Naming
+
+export const Man2="Spider-Man";
+
+
+//for default
+
+export default Man1;
+
---
+++ React-Day01/Import&Export01/script.js
@@ -0,0 +1,13 @@
+
+//for Default
+
+import Insan from "./file.js" //here you give kuch bhi namm
+
+console.log(Insan)
+
+
+//For Naming
+
+import {Man2} from "./file.js" //here you give name that define in export file eg.Man2
+
+console.log(Man2)
